2a5a3190b1b36b43008bc1dfac7e72a1f4954f23
Nitin Rajale <nitinrajale5@gmail.com>
2019-04-07 16:37:04 +0530

    -- initial commit

---
+++ src/main/java/com/employee/SpringBoot2JpaSwaggerApplication.java
@@ -0,0 +1,13 @@
+package com.employee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot2JpaSwaggerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBoot2JpaSwaggerApplication.class, args);
+	}
+
+}
